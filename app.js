0f38e4ccb1672d56eeb602425a003704591af63f
Kit Howlett <kit.howlett@gmail.com>
2021-10-04 16:48:33 +0100

    Practice async function

--- app.js
+++ app.js
@@ -1,26 +1,70 @@
-const got = require('got');
-const apikey = require('./apikey.js');
-const Location = require('./locationClass.js');
+const got = require("got");
+const apikey = require("./apikey.js");
+const Location = require("./locationClass.js");
 
 const locations = [];
 
-const metOfficeData = got
-    .get(`http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=${apikey}`)
-    .json()
-    .then(body => {
-        body.Locations.Location.forEach(locationObject => {
-            const elevation = locationObject.elevation;
-            const id = locationObject.id;
-            const latitude = locationObject.latitude;
-            const longitude = locationObject.longitude;
-            const name = locationObject.name;
-            const region = locationObject.region;
-            const unitaryAuthArea = locationObject.unitaryAuthArea;
+// const metOfficeData = got
+//   .get(
+//     `http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=${apikey}`
+//   )
+//   .json()
+//   .then((body) => {
+//     body.Locations.Location.forEach((locationObject) => {
+//       const elevation = locationObject.elevation;
+//       const id = locationObject.id;
+//       const latitude = locationObject.latitude;
+//       const longitude = locationObject.longitude;
+//       const name = locationObject.name;
+//       const region = locationObject.region;
+//       const unitaryAuthArea = locationObject.unitaryAuthArea;
 
-            locations.push(new Location(elevation, id, latitude, longitude, name, region, unitaryAuthArea));
-        })
-        return locations;
-    })
-    .catch(err => {
-        console.log(err);
-    });
+//       locations.push(
+//         new Location(
+//           elevation,
+//           id,
+//           latitude,
+//           longitude,
+//           name,
+//           region,
+//           unitaryAuthArea
+//         )
+//       );
+//     });
+//     return locations;
+//   })
+//   .catch((err) => {
+//     console.log(err);
+//   });
+
+async function test() {
+  const body = await got
+    .get(
+      `http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=${apikey}`
+    )
+    .json();
+
+  body.Locations.Location.forEach((locationObject) => {
+    const elevation = locationObject.elevation;
+    const id = locationObject.id;
+    const latitude = locationObject.latitude;
+    const longitude = locationObject.longitude;
+    const name = locationObject.name;
+    const region = locationObject.region;
+    const unitaryAuthArea = locationObject.unitaryAuthArea;
+  }
+
+  locations.push(
+    new Location(
+      elevation,
+      id,
+      latitude,
+      longitude,
+      name,
+      region,
+      unitaryAuthArea
+    )
+  );
+}
+
+test(); // puts EVERYTHING after it in a ".then"... kind of
