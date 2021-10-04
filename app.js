5ce83ec01541fa0aba2d4f7e8385ae614ca0c73c
Kit Howlett <kit.howlett@gmail.com>
2021-10-04 15:42:59 +0100

    API data into location array

--- app.js
+++ app.js
@@ -1,14 +1,44 @@
 const got = require('got');
 const apikey = require('./apikey.js');
+const Location = require('./locationClass.js');
+
+const locations = [];
 
 const metOfficeData = got
     .get(`http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=${apikey}`)
     .json()
     .then(body => {
-        body.Locations.Location.forEach(elem => {
-            console.log(elem);
+        body.Locations.Location.forEach(locationObject => {
+            const elevation = locationObject.elevation;
+            const id = locationObject.id;
+            const latitude = locationObject.latitude;
+            const longitude = locationObject.longitude;
+            const name = locationObject.name;
+            const region = locationObject.region;
+            const unitaryAuthArea = locationObject.unitaryAuthArea;
+
+            locations.push(new Location(elevation, id, latitude, longitude, name, region, unitaryAuthArea));
         })
+        return locations;
     })
+    // .then(locationArray => {
+    //     console.log(locationArray);
+    // })
     .catch(err => {
         console.log(err);
     });
+
+console.log(locations[0]);
+
+/* 
+    Location {
+        elevation: 56,
+        id: 72,
+        latitude: 87,
+        longitude: 678,
+        name: 'Surrey',
+        region: 'UK',
+        unitaryAuthArea: 'Red'
+      }
+*/
+    
