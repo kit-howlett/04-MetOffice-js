11ef88b1c1be3d2bc5efc6f779dcc2c248f9fd8c
Kit Howlett <kit.howlett@gmail.com>
2021-10-06 15:46:57 +0100

    App now reads data from metoffice api using query string.
    Co-authored-by: Anya <anya02@users.noreply.github.com>
    Co-authored-by: Kenneth Siu <Kenneth-Siu@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,17 +1,62 @@
+const got = require("got");
 const express = require('express');
 const app = express();
-const port = 3000;
+const apikey = require("./apikey.js");
+const Location = require("./locationClass.js");
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.get('/forecast', function (req, res) {
+  got
+  .get(
+    `http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/sitelist?key=${apikey}`
+  )
+  .json()
+  .then((body) => {
+    const locations = []; 
+
+    body.Locations.Location.forEach((locationObject) => {
+      const elevation = locationObject.elevation;
+      const id = locationObject.id;
+      const latitude = locationObject.latitude;
+      const longitude = locationObject.longitude;
+      const name = locationObject.name;
+      const region = locationObject.region;
+      const unitaryAuthArea = locationObject.unitaryAuthArea;
+
+      locations.push(
+        new Location(
+          elevation,
+          id,
+          latitude,
+          longitude,
+          name,
+          region,
+          unitaryAuthArea
+        )
+      );
+    })
+    return locations;
+  })
+  .then(locations => {
+    const urlLocationName = req.query.locationName;
+    const urlLocationID = getLocationId(urlLocationName, locations);
+    
+    return got
+    .get(`http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/${urlLocationID}?res=daily&key=${apikey}`)
+    .json();
+  })
+  .then(data => {
+    res.send(data.SiteRep.DV);
+  })
+  .catch(err => {
+    res.send(err);
+  }) 
 });
 
-app.get('/foo', (req, res) => {
-    res.send({
-        foo: 'bar'
-    });
+app.listen(3000, () => {
+  console.log(`Example app listening at http://localhost:3000`);
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+function getLocationId(locationName, locationsArray) {
+  const location = locationsArray.find(location => location.name === locationName);
+  return location.id;
+}
